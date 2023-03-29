332880b89c6decfc6e373c981b62f2d2c95f2d56
DanijelaDaca <dacasimic4444@gmail.com>
2023-03-29 10:03:33 +0200

    Add project files.

---
+++ Kosta_Markovic/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kosta_Markovic
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIzlaz_Click(object sender, EventArgs e)
+        {
+          Application.Exit();
+        }
+
+        private void btnBrisanje_Click(object sender, EventArgs e)
+        {
+            txtboxKvadratura.Clear();
+            txtboxLokacija.Clear();
+            cbxGaraza.Checked= false;
+            cbxGrejanje.Checked = false;    
+            cbxKablovska.Checked = false;   
+            cbxLift.Checked= false;
+            richTextBox1.Clear();
+            txtboxLokacija.Focus();
+            
+            
+        }
+
+        private void btnIspis_Click(object sender, EventArgs e)
+        {
+
+            string ispis = "";
+            
+
+            ispis += txtboxLokacija.Text+ "\n";
+            ispis += txtboxKvadratura.Text + "m2"+"\n";
+            if (rbtnKuca.Checked)
+            {
+                ispis += "Kuca"+"\n";
+
+            }
+            else if (rbtnStan.Checked)
+            {
+                ispis += "Stan" + "\n";
+
+            }
+            else 
+            {
+                ispis += "Plac" + "\n";
+
+            }
+           if(cbxGaraza.Checked) { 
+                ispis+="Garaza" + "\n"; 
+            }
+           if(cbxGrejanje.Checked)
+            {
+                ispis += "Grejanje" + "\n";
+
+            }
+            if (cbxLift.Checked)
+                {
+                ispis += "Lift" + "\n";
+
+            }
+            if(cbxKablovska.Checked)
+            {
+                ispis += "Lift" + "\n";
+            }
+            richTextBox1.Text=ispis;
+
+
+
+        }
+    }
+}
---
+++ Kosta_Markovic/Kosta_Markovic.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Kosta_Markovic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtboxLokacija = new System.Windows.Forms.TextBox();
+            this.txtboxKvadratura = new System.Windows.Forms.TextBox();
+            this.btnIspis = new System.Windows.Forms.Button();
+            this.btnBrisanje = new System.Windows.Forms.Button();
+            this.btnIzlaz = new System.Windows.Forms.Button();
+            this.cbxLift = new System.Windows.Forms.CheckBox();
+            this.cbxGaraza = new System.Windows.Forms.CheckBox();
+            this.cbxGrejanje = new System.Windows.Forms.CheckBox();
+            this.cbxKablovska = new System.Windows.Forms.CheckBox();
+            this.rbtnStan = new System.Windows.Forms.RadioButton();
+            this.rbtnKuca = new System.Windows.Forms.RadioButton();
+            this.rbtnPlac = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.gbx1 = new System.Windows.Forms.GroupBox();
+            this.gbx1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lokacija";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kvadratura";
+            // 
+            // txtboxLokacija
+            // 
+            this.txtboxLokacija.Location = new System.Drawing.Point(173, 6);
+            this.txtboxLokacija.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtboxLokacija.Name = "txtboxLokacija";
+            this.txtboxLokacija.Size = new System.Drawing.Size(136, 28);
+            this.txtboxLokacija.TabIndex = 2;
+            // 
+            // txtboxKvadratura
+            // 
+            this.txtboxKvadratura.Location = new System.Drawing.Point(173, 49);
+            this.txtboxKvadratura.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtboxKvadratura.Name = "txtboxKvadratura";
+            this.txtboxKvadratura.Size = new System.Drawing.Size(136, 28);
+            this.txtboxKvadratura.TabIndex = 3;
+            // 
+            // btnIspis
+            // 
+            this.btnIspis.Location = new System.Drawing.Point(534, 221);
+            this.btnIspis.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIspis.Name = "btnIspis";
+            this.btnIspis.Size = new System.Drawing.Size(126, 30);
+            this.btnIspis.TabIndex = 4;
+            this.btnIspis.Text = "Ispis";
+            this.btnIspis.UseVisualStyleBackColor = true;
+            this.btnIspis.Click += new System.EventHandler(this.btnIspis_Click);
+            // 
+            // btnBrisanje
+            // 
+            this.btnBrisanje.Location = new System.Drawing.Point(534, 259);
+            this.btnBrisanje.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBrisanje.Name = "btnBrisanje";
+            this.btnBrisanje.Size = new System.Drawing.Size(126, 33);
+            this.btnBrisanje.TabIndex = 5;
+            this.btnBrisanje.Text = "Brisanje";
+            this.btnBrisanje.UseVisualStyleBackColor = true;
+            this.btnBrisanje.Click += new System.EventHandler(this.btnBrisanje_Click);
+            // 
+            // btnIzlaz
+            // 
+            this.btnIzlaz.Location = new System.Drawing.Point(534, 300);
+            this.btnIzlaz.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIzlaz.Name = "btnIzlaz";
+            this.btnIzlaz.Size = new System.Drawing.Size(126, 31);
+            this.btnIzlaz.TabIndex = 6;
+            this.btnIzlaz.Text = "Izlaz";
+            this.btnIzlaz.UseVisualStyleBackColor = true;
+            this.btnIzlaz.Click += new System.EventHandler(this.btnIzlaz_Click);
+            // 
+            // cbxLift
+            // 
+            this.cbxLift.AutoSize = true;
+            this.cbxLift.Location = new System.Drawing.Point(455, 23);
+            this.cbxLift.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxLift.Name = "cbxLift";
+            this.cbxLift.Size = new System.Drawing.Size(76, 25);
+            this.cbxLift.TabIndex = 7;
+            this.cbxLift.Text = "Lift";
+            this.cbxLift.UseVisualStyleBackColor = true;
+            // 
+            // cbxGaraza
+            // 
+            this.cbxGaraza.AutoSize = true;
+            this.cbxGaraza.Location = new System.Drawing.Point(455, 56);
+            this.cbxGaraza.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxGaraza.Name = "cbxGaraza";
+            this.cbxGaraza.Size = new System.Drawing.Size(98, 25);
+            this.cbxGaraza.TabIndex = 8;
+            this.cbxGaraza.Text = "Garaza";
+            this.cbxGaraza.UseVisualStyleBackColor = true;
+            // 
+            // cbxGrejanje
+            // 
+            this.cbxGrejanje.AutoSize = true;
+            this.cbxGrejanje.Location = new System.Drawing.Point(455, 89);
+            this.cbxGrejanje.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxGrejanje.Name = "cbxGrejanje";
+            this.cbxGrejanje.Size = new System.Drawing.Size(230, 25);
+            this.cbxGrejanje.TabIndex = 9;
+            this.cbxGrejanje.Text = "Centralno grejanje";
+            this.cbxGrejanje.UseVisualStyleBackColor = true;
+            // 
+            // cbxKablovska
+            // 
+            this.cbxKablovska.AutoSize = true;
+            this.cbxKablovska.Location = new System.Drawing.Point(455, 122);
+            this.cbxKablovska.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxKablovska.Name = "cbxKablovska";
+            this.cbxKablovska.Size = new System.Drawing.Size(131, 25);
+            this.cbxKablovska.TabIndex = 10;
+            this.cbxKablovska.Text = "Kablovska";
+            this.cbxKablovska.UseVisualStyleBackColor = true;
+            // 
+            // rbtnStan
+            // 
+            this.rbtnStan.AutoSize = true;
+            this.rbtnStan.Checked = true;
+            this.rbtnStan.Location = new System.Drawing.Point(15, 28);
+            this.rbtnStan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbtnStan.Name = "rbtnStan";
+            this.rbtnStan.Size = new System.Drawing.Size(75, 25);
+            this.rbtnStan.TabIndex = 12;
+            this.rbtnStan.TabStop = true;
+            this.rbtnStan.Text = "Stan";
+            this.rbtnStan.UseVisualStyleBackColor = true;
+            // 
+            // rbtnKuca
+            // 
+            this.rbtnKuca.AutoSize = true;
+            this.rbtnKuca.Location = new System.Drawing.Point(15, 62);
+            this.rbtnKuca.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbtnKuca.Name = "rbtnKuca";
+            this.rbtnKuca.Size = new System.Drawing.Size(75, 25);
+            this.rbtnKuca.TabIndex = 13;
+            this.rbtnKuca.Text = "Kuca";
+            this.rbtnKuca.UseVisualStyleBackColor = true;
+            // 
+            // rbtnPlac
+            // 
+            this.rbtnPlac.AutoSize = true;
+            this.rbtnPlac.Location = new System.Drawing.Point(15, 97);
+            this.rbtnPlac.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbtnPlac.Name = "rbtnPlac";
+            this.rbtnPlac.Size = new System.Drawing.Size(75, 25);
+            this.rbtnPlac.TabIndex = 14;
+            this.rbtnPlac.Text = "Plac";
+            this.rbtnPlac.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(203, 155);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(291, 192);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // gbx1
+            // 
+            this.gbx1.Controls.Add(this.rbtnStan);
+            this.gbx1.Controls.Add(this.rbtnKuca);
+            this.gbx1.Controls.Add(this.rbtnPlac);
+            this.gbx1.Location = new System.Drawing.Point(34, 112);
+            this.gbx1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbx1.Name = "gbx1";
+            this.gbx1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbx1.Size = new System.Drawing.Size(161, 130);
+            this.gbx1.TabIndex = 16;
+            this.gbx1.TabStop = false;
+            this.gbx1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.ClientSize = new System.Drawing.Size(682, 353);
+            this.Controls.Add(this.gbx1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.cbxKablovska);
+            this.Controls.Add(this.cbxGrejanje);
+            this.Controls.Add(this.cbxGaraza);
+            this.Controls.Add(this.cbxLift);
+            this.Controls.Add(this.btnIzlaz);
+            this.Controls.Add(this.btnBrisanje);
+            this.Controls.Add(this.btnIspis);
+            this.Controls.Add(this.txtboxKvadratura);
+            this.Controls.Add(this.txtboxLokacija);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Markovic_Kosta";
+            this.gbx1.ResumeLayout(false);
+            this.gbx1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtboxLokacija;
+        private System.Windows.Forms.TextBox txtboxKvadratura;
+        private System.Windows.Forms.Button btnIspis;
+        private System.Windows.Forms.Button btnBrisanje;
+        private System.Windows.Forms.Button btnIzlaz;
+        private System.Windows.Forms.CheckBox cbxLift;
+        private System.Windows.Forms.CheckBox cbxGaraza;
+        private System.Windows.Forms.CheckBox cbxGrejanje;
+        private System.Windows.Forms.CheckBox cbxKablovska;
+        private System.Windows.Forms.RadioButton rbtnStan;
+        private System.Windows.Forms.RadioButton rbtnKuca;
+        private System.Windows.Forms.RadioButton rbtnPlac;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox gbx1;
+    }
+}
+
